99a42160826106ee984bfacff2f2873823abc8e6
Manish <92672117+manish-xyz@users.noreply.github.com>
2022-12-15 21:31:24 +0530

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
 const path = require('path');
+const postCssPlugins = [
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('autoprefixer'),
+];
 module.exports = {
     entry: './app/assets/scripts/App.js',
     output: {
@@ -7,4 +12,10 @@ module.exports = {
     },
     mode: 'development',
     watch: true,
+    module: {
+        rules: [{
+            test: /\.css$/i,
+            use: ['style-loader','css-loader?url=false', {loader: 'postcss-loader', options: {postcssOptions: {plugins: [postCssPlugins]}}}],
+        }]
+    }
 }
